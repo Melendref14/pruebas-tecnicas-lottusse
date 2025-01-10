f1addc9a2a631f4ab97e9f96cad13f208ef22c70
Frank Reinaldo Melendre Guia <frankmelendre14@gmail.com>
2025-01-10 19:00:48 +0100

    Add unit tests to verify application context loads correctly

---
+++ lottusse-backend/src/test/java/com/example/lottusse_backend/LottuseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lottusse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottuseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

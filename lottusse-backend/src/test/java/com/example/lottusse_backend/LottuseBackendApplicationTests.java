cf31dc99e6040656ca2aa26f2815709bc73f6b3a
Frank Reinaldo Melendre Guia <frankmelendre14@gmail.com>
2025-01-11 17:58:31 +0100

    Add dotenv setup in tests to configure environment variables

--- lottusse-backend/src/test/java/com/example/lottusse_backend/LottuseBackendApplicationTests.java
+++ lottusse-backend/src/test/java/com/example/lottusse_backend/LottuseBackendApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.lottusse_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LottuseBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void setup() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("ALLOWED_ORIGIN", dotenv.get("ALLOWED_ORIGIN"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }

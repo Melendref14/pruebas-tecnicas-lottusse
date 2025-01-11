535fb7fcba2837d58b965f7fecb66af5b70a9aa6
Frank Reinaldo Melendre Guia <frankmelendre14@gmail.com>
2025-01-11 20:13:30 +0100

    Refactor Products component to use custom hook for fetching products and improve layout with logo

--- lottusse-frontend/app/page.js
+++ lottusse-frontend/app/page.js
@@ -1,50 +1,36 @@
 "use client";
 
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import SearchBar from './components/SearchBar';
-import ProductList from './components/ProductList';
+import { useState } from "react";
+import Image from "next/image";
+import SearchBar from "./components/SearchBar";
+import ProductList from "./components/ProductList";
+import useProducts from "./hooks/useProducts";
+import logo from "./assets/logo.svg";
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState(null);
+    const { products, error, loading } = useProducts();
     const [searchName, setSearchName] = useState("");
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`);
-                setProducts(response.data);
-            } catch (error) {
-                console.error('Error fetching products:', error);
-                setError('Error fetching products. Please try again later.');
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []);
 
     const filteredProducts = products.filter(product =>
         product.name.toLowerCase().includes(searchName.toLowerCase())
     );
 
     return (
-        <div className="w-full bg-gray-200 min-h-screen p-8">
-            <div className="container mx-auto grid grid-cols-3 gap-8">
+        <div className="relative bg-mesh w-full min-h-screen p-8">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4">
+                <Image src={logo} alt="Logo" className="w-32 h-auto" />
+            </div>
+            <div className="container mx-auto grid grid-cols-3 gap-8 mt-16">
                 {/* Card izquierdo con Titulo y Subtitulos */}
-                <div className="col-span-1 bg-white p-6 rounded-lg shadow-lg h-auto">
+                <div className="col-span-1 bg-white p-6 rounded-lg shadow-lg h-max">
                     <h1 className="text-4xl font-extrabold text-gray-900 mb-4">Lista de Productos</h1>
-                    <ul className="list-disc ml-5 space-y-2 text-gray-700">
+                    <ul className="list-disc ml-5 space-y-2 text-gray-700 font-semibold">
                         <li>Busca productos por su nombre 🔎</li>
                         <li>Nombre con descripcion detallada de cada producto.. ✨</li>
                         <li>Diseño Responsivo para todos los dispositivos 📱</li>
                     </ul>
                     <SearchBar searchName={searchName} setSearchName={setSearchName} />
                 </div>
-
                 {/* Card derecho para mostrar los productos */}
                 {loading ? (
                     <div className="col-span-2 flex justify-center items-center">
